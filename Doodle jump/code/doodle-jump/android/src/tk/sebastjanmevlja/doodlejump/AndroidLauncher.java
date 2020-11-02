ae55722d95a97c7533671aa3a1f8f3fa24b15f18
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-02 10:51:34 +0100

    Update AndroidLauncher.java
    Don't allow screen dim.

--- Doodle jump/code/doodle-jump/android/src/tk/sebastjanmevlja/doodlejump/AndroidLauncher.java
+++ Doodle jump/code/doodle-jump/android/src/tk/sebastjanmevlja/doodlejump/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tk.sebastjanmevlja.doodlejump.MyGame.Main;
 import tk.sebastjanmevlja.doodlejump.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		config.useWakelock = true; //screen stays on
+		initialize(new Main(), config);
 	}
 }
