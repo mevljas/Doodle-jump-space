2795dd7510be64601f65470f149151ddd62d0f69
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-02 10:51:57 +0100

    Update AndroidLauncher.java
    Code cleanup.

--- Doodle jump/code/doodle-jump/android/src/tk/sebastjanmevlja/doodlejump/AndroidLauncher.java
+++ Doodle jump/code/doodle-jump/android/src/tk/sebastjanmevlja/doodlejump/AndroidLauncher.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import tk.sebastjanmevlja.doodlejump.MyGame.Main;
-import tk.sebastjanmevlja.doodlejump.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
