dfe3fd28445d76424180aef592939ed83835c900
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-29 13:15:23 +0100

    Update Walls.java
    Add second wall.

--- Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Walls.java
+++ Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Walls.java
@@ -4,22 +4,38 @@ import com.badlogic.gdx.physics.box2d.*;
 
 public class Walls {
 
-    private float width = Constants.WIDTH / Constants.PPM;
-    private float height = Constants.HEIGHT/Constants.PPM;
+    private static final float width = Constants.WIDTH / Constants.PPM;
+    private static final float height = Constants.HEIGHT/Constants.PPM;
 
 
 
     public Walls(World world) {
 
-        BodyDef bodyDef2 = new BodyDef();
-        bodyDef2.type = BodyDef.BodyType.StaticBody;
-        bodyDef2.position.set(0,0);
+//        Left wall
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+//        bodyDef.position.set(0,0);
+
         FixtureDef fixtureDef = new FixtureDef();
         EdgeShape edgeShape = new EdgeShape();
-        edgeShape.set(0,0,0,Constants.HEIGHT);
+        edgeShape.set(0,0,0,height);
         fixtureDef.shape = edgeShape;
-        Body bodyEdgeScreen = world.createBody(bodyDef2);
+
+        Body bodyEdgeScreen = world.createBody(bodyDef);
         bodyEdgeScreen.createFixture(fixtureDef);
-        edgeShape.dispose();
+
+
+        //        Right wall
+
+        bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        fixtureDef = new FixtureDef();
+        edgeShape = new EdgeShape();
+        edgeShape.set(width,width,height,height);
+        fixtureDef.shape = edgeShape;
+
+        bodyEdgeScreen = world.createBody(bodyDef);
+        bodyEdgeScreen.createFixture(fixtureDef);
     }
 }
