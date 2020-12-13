f16312b8b54d09d71e563dbe3a999a4ba78bf0cc
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-13 16:49:20 +0100

    Resized player's bounding box.

--- Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Player.java
+++ Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Player.java
@@ -61,7 +61,7 @@ public class Player extends Actor {
         // Now define the dimensions of the physics shape
         PolygonShape shape = new PolygonShape();
         // Basically set the physics polygon to a box with the same dimensions as our sprite
-        shape.setAsBox(sprite.getWidth() / 3 / PPM, sprite.getHeight()
+        shape.setAsBox(sprite.getWidth() / 4 / PPM, sprite.getHeight()
                 /2 / PPM);
         // FixtureDef is a confusing expression for physical properties
         // Basically this is where you, in addition to defining the shape of the body
