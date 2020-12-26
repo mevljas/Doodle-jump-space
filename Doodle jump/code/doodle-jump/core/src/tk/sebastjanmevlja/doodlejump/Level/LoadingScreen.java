0548c70ef829221ffe869496316b2aee270920ba
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-26 21:59:35 +0100

    Code cleanup.

--- Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Level/LoadingScreen.java
+++ Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Level/LoadingScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import tk.sebastjanmevlja.doodlejump.Gameplay.Asset;
 import tk.sebastjanmevlja.doodlejump.Gameplay.Constants;
+import tk.sebastjanmevlja.doodlejump.Gameplay.Sound;
 import tk.sebastjanmevlja.doodlejump.MyGame.Game;
 
 public class LoadingScreen implements Screen {
@@ -38,7 +39,7 @@ public class LoadingScreen implements Screen {
         stage.addActor(progressBar);
         addAssets();
 
-//        Sound.changeMusicState();
+        Sound.changeMusicState();
     }
 
     private void addAssets() {
