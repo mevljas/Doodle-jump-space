52a2a7b11d95fedb8454ba133197ebb00adde28d
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2021-02-23 19:14:53 +0100

    Changed platform max spacing.

--- Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Platform/PlatformFactory.java
+++ Doodle jump/code/doodle-jump/core/src/tk/sebastjanmevlja/doodlejump/Gameplay/Platform/PlatformFactory.java
@@ -30,7 +30,7 @@ public class PlatformFactory {
 
     public PlatformFactory() {
         platforms = new LinkedList<>();
-        maxSpacingHeight = PLATFORM_HEIGHT * 3.6f;
+        maxSpacingHeight = PLATFORM_HEIGHT * 3.4f;
         minSpacingHeight = PLATFORM_HEIGHT * 1.9f;
         maxSpacingWidth = Constants.WIDTH - PLATFORM_WIDTH;
         minSpacingWidth = PLATFORM_WIDTH * 0.1f;
